9f95ef4b465cc4bc99dcfedef968d12f7c63a073
Krisha P. <85227096+krisha-parikh@users.noreply.github.com>
2026-04-22 18:24:19 -0500

    Fix CORS origin URL format

--- server/app/main.py
+++ server/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://incredible-mochi-6de56f.netlify.app/"
+        "https://incredible-mochi-6de56f.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
